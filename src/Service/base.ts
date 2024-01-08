5171cfbeb5ac9bf77c52f3ee604e13c5c818c7c4
Rizwan <104551910+RizwanSidd@users.noreply.github.com>
2024-01-08 22:21:30 +0530

    Update Code

--- src/Service/base.ts
+++ src/Service/base.ts
@@ -1,2 +1,2 @@
-export const baseurl = "http://192.168.1.6:8010/user"
-// export const baseurl = "http://80.209.233.148:8010/"
+// export const baseurl = "http://192.168.1.6:8010/user"
+export const baseurl = "http://80.209.233.148:8010/"
